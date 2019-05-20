2843e01b0bb996ff024c0988ac5d540ef360a808
jihaddeveloper <jihad.developer@gmail.com>
2019-05-20 15:58:44 +0600

    Headers For API Accessing.

--- server.js
+++ server.js
@@ -42,6 +42,37 @@ app.use(bodyParser.json());
 //To see the executed url
 app.use(morgan("dev"));
 
+//Headers For API Accessing
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://eclforum.herokuapp.com/"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+//Headers For API Accessing
+app.use(cors({ origin: "https://eclforum.herokuapp.com/" }));
+
 //MongoDB Connection
 mongoose
   .connect(secret.mongoURI)
