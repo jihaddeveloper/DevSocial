806b32aa92b2f6d433b86e33ac3170b664e028c1
jihaddeveloper <jihad.developer@gmail.com>
2019-05-20 15:03:03 +0600

    Cors added to get API access.

--- server.js
+++ server.js
@@ -4,70 +4,71 @@
 //  Description: Main entry file for rest api project for ECL E-Commerce Forum
 
 //  Library imports
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
 
 //Passport config
-require('./config/passport');
+require("./config/passport");
 
 //Secret tokens
-const secret = require('./config/secret');
+const secret = require("./config/secret");
 
 //Route Controller imports
-const userController = require('./routes/api/userController');
-const postCategoryController = require('./routes/api/postCategoryController');
-const postController = require('./routes/api/postController');
-const profileController = require('./routes/api/profileController');
+const userController = require("./routes/api/userController");
+const postCategoryController = require("./routes/api/postCategoryController");
+const postController = require("./routes/api/postController");
+const profileController = require("./routes/api/profileController");
 
 //Main Application
 const app = express();
 
 //Setup static folder for images and files
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 //Bodyparser Middleware to read from from data
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 
 //To read raw json input.
 app.use(bodyParser.json());
 
 //To see the executed url
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //MongoDB Connection
 mongoose
-    .connect(secret.mongoURI)
-    .then(() => console.log('Conntected to MongoDB'))
-    .catch(err => console.log(err));
-
+  .connect(secret.mongoURI)
+  .then(() => console.log("Conntected to MongoDB"))
+  .catch(err => console.log(err));
 
 //Entery point of the Application
-app.get('/', (req, res) => res.send('Hello World'));
+app.get("/", (req, res) => res.send("Hello World"));
 
 //Using route controller
-app.use('/api/user', userController);
-app.use('/api/profile', profileController);
-app.use('/api/post', postController);
-app.use('/api/postcategory', postCategoryController);
+app.use("/api/user", userController);
+app.use("/api/profile", profileController);
+app.use("/api/post", postController);
+app.use("/api/postcategory", postCategoryController);
 
 //Serve static assets if in production or deployment
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
 
-    //Set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 //Server startup
-app.listen(secret.port, function (err) {
-    if (err) throw err;
-    console.log(`Server is Running on http://localhost:${secret.port}/`);
-})
+app.listen(secret.port, function(err) {
+  if (err) throw err;
+  console.log(`Server is Running on http://localhost:${secret.port}/`);
+});
